2115c89cbc6c59721edaa649884d701f3b79a42d
gabrielimartins <martins.gabrieli.07.almeida@outlook.com>
2023-06-06 11:47:48 -0300

    Aprendendo sobre modulos nativos

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { NativeModules } from 'react-native';
 
 import { Container, Title } from './styles';
 import { Buttom } from '../../components/Buttom';
@@ -8,9 +9,22 @@ import { StackNavigationProp } from '@react-navigation/stack';
 export function Login(){
     const navigation = useNavigation<StackNavigationProp<RootParamList>>();
 
+    const {ReactOneCustomMethod} = NativeModules;
+
+
     function handleLogin(){
         navigation.navigate('home')
     }
+
+    useEffect(() => {
+        ReactOneCustomMethod.getPhoneID()
+      .then((res: string) => {
+        console.log('ID: ' + res);
+      })
+      .catch((err: any) => {
+        console.error(err);
+      });
+    }, [])
 
     return (
         <Container>
